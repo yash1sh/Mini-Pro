b52eab0f1544280bd7dc6939edccd7cb11fa605c
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-06-06 21:31:25 +0530

    Add files via upload
    Added a key code mini pro where you can press and key and get the keycode

---
+++ keyCode/index.js
@@ -0,0 +1,18 @@
+const insert = document.querySelector(".container");
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `
+  <div class="key">
+  ${e.key === " " ? "Space" : e.key}
+  <small>event.key</small>
+</div>
+<div class="key">
+  ${e.keyCode}
+  <small>event.KeyCode</small>
+</div>
+<div class="key">
+  ${e.code}
+  <small>event.code</small>
+</div>
+  `;
+});
