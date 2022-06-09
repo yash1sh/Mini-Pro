17766b0283aec042520a7421c3a5c7817b8eb869
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-06-09 21:44:21 +0530

    Update index.js
    deleted toggling the extra class

--- FAQ/index.js
+++ FAQ/index.js
@@ -3,7 +3,6 @@ const boxs = document.querySelectorAll(".box");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    button.parentNode.classList.toggle("tran");
     button.parentNode.classList.toggle("active");
     if (button.innerText == "X") button.innerHTML = "V";
     else button.innerHTML = "X";
