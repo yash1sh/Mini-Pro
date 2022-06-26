00c91216d5badfcdb9319393de06eae6607d987c
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-06-26 18:20:20 +0530

    Add files via upload
    added new without js

---
+++ DoubleVericalSlider/index.js
@@ -0,0 +1,5 @@
+const sideleft = document.querySelector(".left-side");
+const sideright = document.querySelector(".right-side");
+const down = document.querySelector(".right");
+const up = document.querySelector(".left");
+const slides = sideright.querySelectorAll(".img").length;
